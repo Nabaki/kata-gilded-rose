623f2402adac9846bc442f21469d4a7ae67a58be
Nabaki <duchet.l@hotmail.fr>
2023-03-27 22:13:51 +0200

    Test code with full coverage

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ Le propriétaire d'une boutique peu ordinaire a fait appel à vous pour ajouter 
 * Le propriétaire ne saurait plus dire comment chacun se gère mais il assure qu'il ne veut aucune modification du comportement existant.
 * La classe Item ne doit pas être modifiée car elle ne lui appartient pas.
 * Un nouvel objet "Conjured" doit pouvoir être géré dans la boutique.
+
+## Références
+* https://github.com/emilybache/GildedRose-Refactoring-Kata/tree/main/Java
+* https://www.youtube.com/watch?v=zyM2Ep28ED8
+
+## Etape 1 : tests
+* Launching tests with coverage
+* Approval tests
+* Mutation tests
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,25 @@
 package com.gildedrose;
 
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
-        GildedRose app = new GildedRose(items);
+    void updateQuality() {
+        CombinationApprovals.verifyAllCombinations(
+            this::doUpdateQuality,
+            new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+            new Integer[]{-1, 0, 5, 6, 10, 11},
+            new Integer[]{0, 1, 49, 50}
+        );
+    }
+
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        Item item = new Item(name, sellIn, quality);
+        GildedRose app = new GildedRose(new Item[]{item});
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        return app.items[0].toString();
     }
 
 }
